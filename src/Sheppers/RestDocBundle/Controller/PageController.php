b031787d333f3b2bed90fb71e1e3b241fc7d7ca6
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2013-06-13 16:38:31 +0100

    Added pages support and the ability to override navigation with a view.

---
+++ src/Sheppers/RestDocBundle/Controller/PageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sheppers\RestDocBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use dflydev\markdown\MarkdownParser;
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/", name="RestDoc_Pages_getPages")
+     * @Method({"GET"})
+     */
+    public function getPagesAction(Request $request)
+    {
+        return $this->getPageAction($request, 'index');
+    }
+
+    /**
+     * @Route("/{page}", name="RestDoc_Pages_getPage")
+     * @Method({"GET"})
+     */
+    public function getPageAction(Request $request, $page)
+    {
+        $file = __DIR__ . '/../Resources/doc/' . $page . '.md';
+        if (false === is_file($file)) {
+            throw $this->createNotFoundException("Page '{$page}' was not found at '{$file}'");
+        }
+
+        $markdownParser = new MarkdownParser();
+
+        return $this->render('SheppersRestDocBundle:Page:getPage.html.twig', array(
+            'content' => $markdownParser->transformMarkdown(file_get_contents($file))
+        ));
+    }
+}
