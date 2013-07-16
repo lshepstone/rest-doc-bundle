32f4d83860e43a92c956b71adbd1ba7c7b75950e
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2013-07-16 11:06:00 +0100

    Updated response prototype to show the first response code, instead of the static 200 OK.

--- src/Sheppers/RestDocBundle/Prototype/ResponsePrototype.php
+++ src/Sheppers/RestDocBundle/Prototype/ResponsePrototype.php
@@ -15,9 +15,25 @@ class ResponsePrototype
 
     public function getHeaders()
     {
+
+
         return array(
             'Content-Type' => 'application/json'
         );
+    }
+
+    public function getStatusLine()
+    {
+        $code = '200';
+        $phrase = 'OK';
+        $responses = $this->operation->getResponses();
+        if (count($responses)) {
+            reset($responses);
+            $code = $responses[0]->getCode();
+            $phrase = $responses[0]->getMessage();
+        }
+
+        return "HTTP/1.1 {$code} {$phrase}";
     }
 
     public function getBody()
